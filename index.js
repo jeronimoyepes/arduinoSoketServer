dccfaa6b022a9c5d6d5257d642bb233e88650c29
Jeronimo <jeronimo_y_g@hotmail.com>
2022-11-12 17:36:15 -0500

    Jero.Y: Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// ↓ cambiar el nombre del puerto al adecuado ↓
+const arduinoComPort = 'COM13'
+
+// Librería para leer el puerto serial
+const { SerialPort } = require('serialport')
+
+// Importar el analizador gramatical para los datos que vienen del arduino
+const { ReadlineParser } = require('@serialport/parser-readline')
+
+// Instanciar un nuevo puerto serial
+const port = new SerialPort({ path: arduinoComPort, baudRate: 9600 })
+
+// Pasar la información del puerto através del analizador gramatical
+const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+
+// En caso de errores en el puerto
+port.on('error', function(err) { console.log('Error: ', err.message); })
+
+// Conexión establecida correctamente con el puerto
+port.on("open", () => {
+  console.log('serial port open');
+});
+
+// Datos provenientes del puerto
+parser.on('data', data =>{
+  console.log('got word from arduino:', data);
+});
